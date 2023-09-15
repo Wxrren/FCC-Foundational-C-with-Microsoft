f125ebde5a6faf2f6e66138098f0a75a0c00c032
Warren Smith <131816114+Wxrren@users.noreply.github.com>
2023-09-16 00:08:49 +0100

    Call Methods From the .NET Class Library Using C#

--- Create and Run Simple C# Console Applications/Program.cs
+++ Create and Run Simple C# Console Applications/Program.cs
@@ -1,16 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+// Task 1 - Make a Dice role method
+Random dice = new Random();
+int roll = dice.Next(1, 7);
+Console.WriteLine(roll);
 
-namespace Learning_C_
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
+// Task 2 - Make a dice roll method that rolls 3 times. Make sure 1 has 0 input.
+Random dice = new Random();
+int roll1 = dice.Next();
+int roll2 = dice.Next(101);
+int roll3 = dice.Next(50, 101);
 
-        }
-    }
-}
+Console.WriteLine($"First roll: {roll1}");
+Console.WriteLine($"Second roll: {roll2}");
+Console.WriteLine($"Third roll: {roll3}");
+
+// Task 3 - Code Challenge: Implement a method of the Math class that returns the larger of two numbers
+int firstValue = 500;
+int secondValue = 600;
+int largerValue = Math.Max(firstValue,secondValue);
+
+Console.WriteLine(largerValue);
