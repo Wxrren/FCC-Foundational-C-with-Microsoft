9508f947a6f4bd6e0b96e4fd4300239fd88e2842
Warren Smith <131816114+Wxrren@users.noreply.github.com>
2023-09-17 13:48:05 +0100

    Add files via upload

--- Create and Run Simple C# Console Applications/Program.cs
+++ Create and Run Simple C# Console Applications/Program.cs
@@ -1,21 +1,108 @@
-// Task 1 - Make a Dice role method
+﻿// Task 1 - Create decision logic with if statements
+
 Random dice = new Random();
-int roll = dice.Next(1, 7);
-Console.WriteLine(roll);
 
-// Task 2 - Make a dice roll method that rolls 3 times. Make sure 1 has 0 input.
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
+
+int total = roll1 + roll2 + roll3;
+
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+if (total >= 15)
+{
+    Console.WriteLine("You win!");
+}
+if (total < 15)
+{
+    Console.WriteLine("Sorry, You lose.");
+}
+
+// Task 1.1 - Add another if statement to implement the doubles bonus
+if ((roll1 == roll2)  (roll2 == roll3)  (roll1 == roll3))
+{
+    Console.WriteLine("You rolled doubles! +2 bonus to total!");
+    total += 2;
+}
+
+// Task 1.2 - Add another if statement to implement the triples bonus
+if ((roll1 == roll2) && (roll2 == roll3))
+{
+    Console.WriteLine("You rolled triples! +6 bonus to total!");
+    total += 6;
+}
+
+// Task 2 - Create nested decision logic with if, else if, and else
+
 Random dice = new Random();
-int roll1 = dice.Next();
-int roll2 = dice.Next(101);
-int roll3 = dice.Next(50, 101);
 
-Console.WriteLine($"First roll: {roll1}");
-Console.WriteLine($"Second roll: {roll2}");
-Console.WriteLine($"Third roll: {roll3}");
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
 
-// Task 3 - Code Challenge: Implement a method of the Math class that returns the larger of two numbers
-int firstValue = 500;
-int secondValue = 600;
-int largerValue = Math.Max(firstValue,secondValue);
+int total = roll1 + roll2 + roll3;
 
-Console.WriteLine(largerValue);
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+
+if ((roll1 == roll2)  (roll2 == roll3)  (roll1 == roll3))
+{
+    if ((roll1 == roll2) && (roll2 == roll3)) 
+    {
+        Console.WriteLine("You rolled triples! +6 bonus to total!");
+        total += 6;
+    }
+    else
+    {
+        Console.WriteLine("You rolled doubles! +2 bonus to total!");
+        total += 2;
+    }
+}
+
+if (total >= 16)
+{
+    Console.WriteLine("You win a new car!");
+}
+else if (total >= 10)
+{
+    Console.WriteLine("You win a new laptop!");
+}
+else if (total == 7)
+{
+    Console.WriteLine("You win a trip for two!");
+}
+else 
+{
+    Console.WriteLine("You win a kitten!");
+}
+
+// Task 3 coding challenge
+Random random = new Random();
+int daysUntilExpiration = random.next(12);
+int discountPercentage = 0;
+
+if (daysUntilExpiration == 0)
+{
+    Console.WriteLine("Your subscription has expired.");
+}
+else if (daysUntilExpiration == 1)
+{
+    Console.WriteLine($"Your suscription ends in {daysUntilExpiration} days");
+    discountPercentage = 20%;
+}
+else if (daysUntilExpiration <= 5)
+{
+    Console.WriteLine($"Your suscription ends in {daysUntilExpiration} days");
+    discountPercentage = 10%;
+}
+else if (daysUntilExpiration <= 10)
+{
+    Console.WriteLine("Your subscription will expire soon. Renew now!")
+}
+if (discountPercentage > 0)
+{
+    Console.WriteLine($"Renew now and save {discountPercentage}%.");
+}
+
+
