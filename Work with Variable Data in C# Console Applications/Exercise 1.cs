5a365c4bd2cfac21322bae09f4a6414739b46be2
Warren Smith <131816114+Wxrren@users.noreply.github.com>
2023-10-24 12:23:44 +0100

    Add files via upload

---
+++ Work with Variable Data in C# Console Applications/Exercise 1.cs
@@ -0,0 +1,34 @@
+// Task 1
+
+Console.WriteLine("Signed integral types:");
+
+Console.WriteLine($"sbyte : {sbyte.MinValue} to {sbyte.MaxValue}");
+Console.WriteLine($"short : {short.MinValue} to {short.MaxValue}");
+Console.WriteLine($"int   : {int.MinValue} to {int.MaxValue}");
+Console.WriteLine($"long  : {long.MinValue} to {long.MaxValue}");
+
+Console.WriteLine("Unsigned integral types:");
+
+Console.WriteLine($"sbyte : {byte.MinValue} to {byte.MaxValue}");
+Console.WriteLine($"short : {ushort.MinValue} to {ushort.MaxValue}");
+Console.WriteLine($"int   : {uint.MinValue} to {uint.MaxValue}");
+Console.WriteLine($"long  : {ulong.MinValue} to {ulong.MaxValue}");
+
+// Task 2 
+Console.WriteLine("");
+Console.WriteLine("Floating point types");
+Console.WriteLine($"float  : {float.MinValue} to {float.MaxValue} (With ~6-9 digits of precision)");
+Console.WriteLine($"double : {double.MinValue} to {double.MaxValue} (with ~15-17 digits of precision)");
+Console.WriteLine($"decimal: {decimal.MinValue} to {decimal.MaxValue} (with 28-29 digits of precision");
+
+//Task 3
+int[] data;
+data = new int[3];
+
+// Modify the above code so it performs on one line
+
+int[] data = new int[3];
+
+string shortenedString = "Hello World";
+Console.WriteLine(shortenedString);
+
